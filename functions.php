448fbd80ab8e5d12261c4a17f283321731845d0d
Nic Ford <nicdford@gmail.com>
2018-07-30 12:07:44 -0700

    Include extras.php

--- functions.php
+++ functions.php
@@ -8,5 +8,8 @@ define( 'FL_CHILD_THEME_URL', get_stylesheet_directory_uri() );
 require_once 'classes/class-fl-child-theme.php';
 require_once 'classes/class-shortcodes.php';
 
+// Includes
+require_once 'includes/extras.php';
+
 // Actions
 add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
